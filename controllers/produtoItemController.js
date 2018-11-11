30e918d71a92bf7f83868cd1faa88a2b37310784
Luís Coelho 1160754 <1160754@isep.ipp.pt>
2018-11-11 20:37:41 +0000

    Atualizacoes

--- controllers/produtoItemController.js
+++ controllers/produtoItemController.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 var ProdutoItem = require('../models/produtoItemModel');
+var async = require("async");
+var Client = require('node-rest-client').Client;
+var client = new Client();
 //var ProdutoItem = mongoose.model(ProdutoItem);
+var baseUrl = "https://sic20181106055047.azurewebsites.net/api/";
+var produtoUrl = baseUrl + "produto/"
 
 exports.findAll = function(req, res, next){
     ProdutoItem.find({}).then(function(items){
@@ -9,7 +14,7 @@ exports.findAll = function(req, res, next){
 };
 
 exports.createItem = function(req, res, next){
-  /**  var item = new ProdutoItem({
+    var item = new ProdutoItem({
         idProduto : req.body.idProduto,
         altura : req.body.altura,
         largura : req.body.largura,
@@ -18,18 +23,12 @@ exports.createItem = function(req, res, next){
         idAcabamento: req.body.idAcabamento,
         items:req.body.items
     });
-    item.save(function(err) {
-        if(err){
-            return next(err);
-        }
-        res.send('item');
-    })
-    /**/
-   /** */
-   ProdutoItem.create(req.body).then(function(item){
-        res.send(item);
-   }).catch(next);
-   /**/
+    itemValidator(item).then(function(value){ ProdutoItem.create(req.body).then(function(item){
+            res.send(item);
+    }
+       )}).catch(next);
+
+    
 };
 
 exports.deleteProdutoItem = function(req, res, next){
@@ -43,3 +42,37 @@ exports.findbyId = function(req, res, next) {
         res.send(item);
     }).catch(next);
 }
+
+function itemValidator(value){
+    return new  Promise(function(resolve, reject){
+            
+        var urlaux = baseUrl + "produto/validar/" + value.idProduto 
+                + "/altura=" + value.altura
+                + "&largura=" + value.largura
+                + "&profundidade=" + value.profundidade
+                + "&idMaterial=" + value.idMaterial
+                + "&idAcabamento=" + value.idAcabamento;
+
+                console.log(urlaux);
+
+        client.get(urlaux, function(data, response){
+            if(response.statusCode == 200){
+                resolve(value);
+            }else{
+                //resolve(false);
+                reject(new Error('produto invalido'));
+            }
+        });
+    });
+}
+/*
+function filhosValidator(value) {
+    var filhos = value.filhos;
+    var aux;
+    for(var i = 0, size = filhos.length; i < size ; i++){
+        aux[i] = ProdutoItem.findById({_id: req.params.id}).then(function(item){
+            
+        }).catch(next);
+    }
+}
+*/
--- models/produtoItemModel.js
+++ models/produtoItemModel.js
@@ -1,19 +1,20 @@
 var mongoose = require('mongoose');
-var uniqueValidator = require ('mongoose-unique-validator');
+//var uniqueValidator = require ('mongoose-unique-validator');
 var idvalidator = require('mongoose-id-validator');
-
 var Schema = mongoose.Schema;
 
+
 var ProdutoItem= new Schema({
-    idProduto : Number,
-    altura : {type: Number, min: 0},
-    largura : {type: Number, min: 0},
-    profundidade: {type: Number, min: 0},
-    idMaterial: Number,
-    idAcabamento: Number,
-    itemPai:{type: Schema.Types.ObjectId, ref: 'ProdutoItem'} 
+    idProduto : {type : String,required : true},
+    altura : {type: Number, min: 0, required : true},
+    largura : {type: Number, min: 0, required : true},
+    profundidade: {type: Number, min: 0, required : true},
+    idMaterial: {type : String, required : true},
+    idAcabamento: {type : String, required : true},
+    filhos:[{type: Schema.Types.ObjectId, ref: 'ProdutoItem'}]
 });
 
-ProdutoItem.plugin(uniqueValidator); 
+//ProdutoItem.plugin(uniqueValidator); 
 ProdutoItem.plugin(idvalidator);
+
 module.exports = mongoose.model('ProdutoItem', ProdutoItem);
